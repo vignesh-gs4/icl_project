e76bcb7b78be3b0e5ea17b43d05e736082daca98
vignesh-gs4 <vignesh04gs@gmail.com>
2026-03-20 11:09:43 +0530

    host changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/icl_project/frontend",
   plugins: [
     react(),
     tailwindcss()
